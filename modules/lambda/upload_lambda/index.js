4ad8dffae25f35a274695c0a06e8bd33fe3213d6
RokwonK <wonrok97@naver.com>
2023-01-10 00:48:06 +0900

    ✨ S3 업로드 로직 및 S3 bucket name 환경변수 사용 추가

--- modules/lambda/upload_lambda/index.js
+++ modules/lambda/upload_lambda/index.js
@@ -1,12 +1,14 @@
-// const AWS = require("aws-sdk");
-// const REGION = "ap-northeast-2"
-// const S3 = new AWS.S3({ region: REGION })
 const multipart = require('parse-multipart');
+const AWS = require('aws-sdk');
+const REGION = "ap-northeast-2"
+const S3 = new AWS.S3({ region: REGION })
 
 exports.handler = async (event, context, callback) => {
+  // Get metadata
   const request = event.Records[0].cf.request;
   const method = request.method;
   const contentType = request.headers['content-type'][0].value;
+  const bucketName = request.origin.s3.customHeaders['x-env-bucket'][0].value;
 
   if (method != 'POST' && !contentType.startsWith('multipart/form-data')) {
     callback(null, {
@@ -14,24 +16,46 @@ exports.handler = async (event, context, callback) => {
       body: JSON.stringify({
         'message': 'wrong access'
       })
-    })
+    });
     return;
   }
 
-  console.log(`Get ContentType ${contentType}`);
-  console.log(`Get headers ${request.headers}`);
+  // Get Multipart
   const bodyBuffer = Buffer.from(request.body.data, 'base64');
   const boundary = multipart.getBoundary(contentType);
   const parts = multipart.Parse(bodyBuffer, boundary);
+  const fileData = parts[0];
+  console.log(`Get Data Info ${fileData.filename} ${fileData.type} ${bucketName}`);
 
-  console.log(`Get Part ${parts[0].data.toString()}`);
-  console.log(request.headers);
-
-  const response = {
-    status: '200',
-    statusDescription: 'OK',
-    body: JSON.stringify({ data: parts[0].data.toString() }),
+  // Set Upload Params
+  let uploadParams = {
+    Bucket: process.env.bucket,
+    Key: `uploads/${fileData.filename}`,
+    Body: fileData.data,
+    ContentEncoding: 'base64',
+    ContentType: fileData.type
   };
 
-  callback(null, response);
+  // Upload & Callback
+  let response;
+  try {
+    const s3Response = await S3.putObject(uploadParams).promise();
+    console.log('Get Response');
+    console.log(s3Response);
+    response = {
+      status: '200',
+      statusDescription: 'OK',
+      body: JSON.stringify({ message: 'upload success' }),
+    };
+  } catch (error) {
+    console.log('Get Error');
+    console.log(error);
+    response = {
+      status: '404',
+      statusDescription: 'OK',
+      body: JSON.stringify({ message: error }),
+    };
+  } finally {
+    callback(null, response);
+  }
 };
