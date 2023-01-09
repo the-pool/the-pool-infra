b39f0258a684ee7a3bc589d2de828183eb5423a2
RokwonK <wonrok97@naver.com>
2023-01-09 23:20:46 +0900

    ✨ upload_lambda multipart 처리 로직 추가

--- modules/lambda/upload_lambda/index.js
+++ modules/lambda/upload_lambda/index.js
@@ -1,33 +1,36 @@
 // const AWS = require("aws-sdk");
 // const REGION = "ap-northeast-2"
 // const S3 = new AWS.S3({ region: REGION })
+const multipart = require('parse-multipart');
 
 exports.handler = async (event, context, callback) => {
-  console.log("Lambda Execute");
-  console.log("Request Event:" + JSON.stringify(event, null, 2));
-  const requestHeaders = event.Records[0].cf.request.headers;
+  const request = event.Records[0].cf.request;
+  const method = request.method;
+  const contentType = request.headers['content-type'][0].value;
+
+  if (method != 'POST' && !contentType.startsWith('multipart/form-data')) {
+    callback(null, {
+      status: '404',
+      body: JSON.stringify({
+        'message': 'wrong access'
+      })
+    })
+    return;
+  }
+
+  console.log(`Get ContentType ${contentType}`);
+  console.log(`Get headers ${request.headers}`);
+  const bodyBuffer = Buffer.from(request.body.data, 'base64');
+  const boundary = multipart.getBoundary(contentType);
+  const parts = multipart.Parse(bodyBuffer, boundary);
+
+  console.log(`Get Part ${parts[0].data.toString()}`);
+  console.log(request.headers);
+
   const response = {
     status: '200',
     statusDescription: 'OK',
-    headers: {
-      'cache-control': [{
-        key: 'Cache-Control',
-        value: 'max-age=100'
-      }],
-      'content-type': [{
-        key: 'Content-Type',
-        value: 'text/html'
-      }],
-      'content-encoding': [{
-        key: 'Content-Encoding',
-        value: 'UTF-8'
-      }],
-    },
-    body: JSON.stringify({
-      code: 200,
-      message: "list data test",
-      data: "Get Get Get"
-    }),
+    body: JSON.stringify({ data: parts[0].data.toString() }),
   };
 
   callback(null, response);
